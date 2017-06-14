3a1cfdbf902c589d2741b038ece07c1b8c5cec25
Charlecia Martin <charlecia.d.martin@gmail.com>
2017-06-13 22:29:57 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/3.7.3/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCcc66_z43kjmpeTsb22IuehM8Jr6ukoKM",
+    authDomain: "train-schedule-7c2c0.firebaseapp.com",
+    databaseURL: "https://train-schedule-7c2c0.firebaseio.com",
+    storageBucket: "train-schedule-7c2c0.appspot.com",
+    messagingSenderId: "886857417701"
+  };
+  firebase.initializeApp(config);
+</script>
